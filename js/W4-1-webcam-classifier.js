1aa4b53abb140453b1f7718ec0f87a06815fcb7e
Farah Amri <farah.amri@student.ehb.be>
2021-03-16 15:28:41 +0100

    oefening 1

--- js/W4-1-webcam-classifier.js
+++ js/W4-1-webcam-classifier.js
@@ -5,21 +5,32 @@ let video;
 let label;
 
 function modelReady() {
-  console.log('Model is ready!');
+  console.log("Model is ready!");
+  mobilenet.predict(gotResults);
 }
 
 function gotResults(error, results) {
-    console.log('Prediction ready');
+    console.log("Prediction ready");
+    const prediction = results[0];
+    if(prediction.probability> 0.7){
+      document.getElementById("label").innerHTML = prediction.className;
+      console.log(results);
+    }
+
+    mobilenet.predict(gotResults);
+    //console.log(results);
 }
 
-// function imageReady() {
-//   image(puffin, 0, 0, width, height);
-// }
+function imageReady() {
+    image(puffin, 0, 0, width, height);
+ }
+
 
 function setup() {
   let canvas = createCanvas(640, 550);
   canvas.parent("container");
   video = createCapture(VIDEO);
+  mobilenet = ml5.imageClassifier("MobileNet",video, modelReady);
   video.hide();
   background(0);
 }
